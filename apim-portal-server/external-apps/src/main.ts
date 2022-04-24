6408fdd395a569e58ff05c20899902ef97df5801
Ricardo Gomez_Ulmke <ricardojosegomezulmke@gmail.com>
2022-04-24 16:14:14 +0200

    x

--- apim-portal-server/external-apps/src/main.ts
+++ apim-portal-server/external-apps/src/main.ts
@@ -4,7 +4,7 @@ import AsOrganizationService from '../as-lib/AsOrganizationService';
 import AsExternalSystemsService from '../as-lib/AsExternalSystemsService';
 import AsUtils from '../as-lib/AsUtils';
 import AsLoginService from '../as-lib/AsLoginService';
-import { APIProduct, ApiProductsService, App, AppsService } from '@solace-iot-team/apim-connector-openapi-node';
+import { APIProduct, ApiProductsService, App, AppsService, AppStatus } from '@solace-iot-team/apim-connector-openapi-node';
 
 const ModuleName = 'aps-external-apps'
 const ComponentName = 'main';
@@ -22,89 +22,133 @@ const validateConfigReferences = async() => {
   
 }
 
-// const importData = async() => {
-//   const funcName = 'importData';
-//   const logName = `${ModuleName}.${ComponentName}.${funcName}()`;
+const create_user_app = async({ userId, createAppRequestBody, appStatus }:{
+  userId: string;
+  createAppRequestBody: App;
+  appStatus: AppStatus;
+}): Promise<void> => {
+  await AppsService.createDeveloperApp({
+    organizationName: Config.getOrganizationId(),
+    developerUsername: userId,
+    requestBody: createAppRequestBody
+  });
+  await AppsService.updateDeveloperApp({
+    organizationName: Config.getOrganizationId(),
+    appName: createAppRequestBody.name,
+    developerUsername: userId,
+    requestBody: {
+      status: appStatus
+    }
+  });
+}
 
+const create_team_app = async({ teamId, createAppRequestBody, appStatus }:{
+  teamId: string;
+  createAppRequestBody: App;
+  appStatus: AppStatus;
+}): Promise<void> => {
+  await AppsService.createTeamApp({
+    organizationName: Config.getOrganizationId(),
+    teamName: teamId,
+    requestBody: createAppRequestBody
+  });
+  await AppsService.updateTeamApp({
+    organizationName: Config.getOrganizationId(),
+    appName: createAppRequestBody.name,
+    teamName: teamId,
+    requestBody: {
+      status: appStatus
+    }
+  });
+}
 
-//   const businessGroupData: any = AsUtils.readFileContentsAsJson(Config.getBusinessGroupImportFilePath());
-//   // console.log(`${logName}: businessGroupAny = ${JSON.stringify(businessGroupAny)}`);
-
-//   const importer: ExternalBusinessGroupImporter = ExternalBusinessGroupImporterFactory.create_ExternalBusinessGroupImporter({
-//     organizationId: Config.getOrganizationId(),
-//     externalSystemId: Config.getExternalSystemId(),
-//     businessGroupData: businessGroupData
-//   });
-
-//   const v: void = await importer.parse();
-//   const vv: void = await importer.present();
-
-// }
-
-
-const create_user_apps = async() => {
-  const funcName = 'create_user_apps';
+const create_apps = async() => {
+  const funcName = 'create_apps';
   const logName = `${ModuleName}.${ComponentName}.${funcName}()`;
 
   // get the first two api products
-  const apiProductList: Array<APIProduct> = await ApiProductsService.listApiProducts({
+  const complete_ApiProductList: Array<APIProduct> = await ApiProductsService.listApiProducts({
     organizationName: Config.getOrganizationId(),
-    pageNumber: 1,
-    pageSize: 2
   });
   // console.log(`${logName}: apiProductList.length=${apiProductList.length}`);
-
-  const apiProductIdList: Array<string> = apiProductList.map( (apiProduct: APIProduct) => {
-    return apiProduct.name;
+  // find 2 api products, one manual, one auto
+  const manual_ApiProduct: APIProduct | undefined = complete_ApiProductList.find( (x) => {
+    return x.approvalType === APIProduct.approvalType.MANUAL;
   });
+  if(manual_ApiProduct === undefined) throw new Error(`${logName}: cannot find a manual approval API product`);
+  const auto_ApiProduct: APIProduct | undefined = complete_ApiProductList.find( (x) => {
+    return x.approvalType === APIProduct.approvalType.AUTO;
+  });
+  if(auto_ApiProduct === undefined) throw new Error(`${logName}: cannot find a auto approval API product`);
+
+  const apiProductIdList: Array<string> = [
+    manual_ApiProduct.name, 
+    auto_ApiProduct.name
+  ];
 
   const userId1: string = "user.one@devel.test";
   const userId2: string = "user.two@devel.test";
+  const userId3: string = "user.three@devel.test";
   const userAppId1: string = 'external-user-app-1_at_' + Date.now();
   const userAppId2: string = 'external-user-app-2_at_' + Date.now();
+  const userAppId3: string = 'external-user-app-3_at_' + Date.now();
   
   const teamId1: string = "team-one";
   const teamId2: string = "team-two";
+  const teamId3: string = "team-three";
   const teamAppId1: string = 'external-team-app-1_at_' + Date.now();
   const teamAppId2: string = 'external-team-app-2_at_' + Date.now();
-
+  const teamAppId3: string = 'external-team-app-3_at_' + Date.now();
 
   const expiresIn: number = 3600; // 1 hour
 
-  // create user app 1
   const appRequestBody: App = {
     name: userAppId1,
     expiresIn: expiresIn,
     apiProducts: apiProductIdList,
     credentials: {}
-  }
-  await AppsService.createDeveloperApp({
-    organizationName: Config.getOrganizationId(),
-    developerUsername: userId1,
-    requestBody: appRequestBody
+  };
+  // create user app 1
+  await create_user_app({ 
+    userId: userId1, 
+    appStatus: AppStatus.APPROVED,
+    createAppRequestBody: appRequestBody
   });
   // create user app 2
   appRequestBody.name = userAppId2;
-  await AppsService.createDeveloperApp({
-    organizationName: Config.getOrganizationId(),
-    developerUsername: userId2,
-    requestBody: appRequestBody
+  await create_user_app({
+    userId: userId2, 
+    appStatus: AppStatus.PENDING,
+    createAppRequestBody: appRequestBody
+  });
+  // create user app 3
+  appRequestBody.name = userAppId3;
+  await create_user_app({
+    userId: userId3, 
+    appStatus: AppStatus.REVOKED,
+    createAppRequestBody: appRequestBody
   });
   // create team app 1
   appRequestBody.name = teamAppId1;
-  await AppsService.createTeamApp({
-    organizationName: Config.getOrganizationId(),
-    teamName: teamId1,
-    requestBody: appRequestBody
+  await create_team_app({
+    teamId: teamId1,
+    appStatus: AppStatus.APPROVED,
+    createAppRequestBody: appRequestBody
   });
   // create team app 2
   appRequestBody.name = teamAppId2;
-  await AppsService.createTeamApp({
-    organizationName: Config.getOrganizationId(),
-    teamName: teamId2,
-    requestBody: appRequestBody
+  await create_team_app({
+    teamId: teamId2,
+    appStatus: AppStatus.PENDING,
+    createAppRequestBody: appRequestBody
   });
-
+  // create team app 3
+  appRequestBody.name = teamAppId3;
+  await create_team_app({
+    teamId: teamId3,
+    appStatus: AppStatus.REVOKED,
+    createAppRequestBody: appRequestBody
+  });
 
 }
 
@@ -123,7 +167,7 @@ const main = async() => {
 
   await validateConfigReferences();
 
-  await create_user_apps();
+  await create_apps();
 
   console.log(`${logName}: done.`);
 }
